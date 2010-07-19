08379a99a9a3f551f4695bb9690676f2678a5a0c
allen-t <kex.allen13@gmail.com>
2010-07-19 12:22:40 +0200

    Trying to resolve the dash/underscore issue.

--- install.rb
+++ install.rb
@@ -1,7 +1,7 @@
 # Install grid controller by creating a link to it
 
 FileUtils.ln_s(
-  File.join(RAILS_ROOT,"vendor/plugins/rails_jqgrid/app/controllers/grid_data_controller.rb"),
+  File.join(RAILS_ROOT,"vendor/plugins/rails-jqgrid/app/controllers/grid_data_controller.rb"),
   File.join(RAILS_ROOT,"app/controllers"),
   :verbose => true
 )
