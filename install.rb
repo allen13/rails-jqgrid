b6821c49d6060cc86f3b6d7faaf524132b3cacc2
allen-t <kex.allen13@gmail.com>
2010-07-26 11:54:39 +0200

    Got rid of symlinks for the copy procedure, as they are a bad idea. Moving from one site to another breaks the link. Also added script for uinstalling the files.

--- install.rb
+++ install.rb
@@ -1,18 +1,18 @@
 # Install grid controller by creating a link to it
 
-FileUtils.ln_s(
+FileUtils.cp(
   File.join(RAILS_ROOT,"vendor/plugins/rails-jqgrid/app/controllers/grid_data_controller.rb"),
   File.join(RAILS_ROOT,"app/controllers"),
   :verbose => true
 )
 
-FileUtils.ln_s(
+FileUtils.cp(
   File.join(RAILS_ROOT,"vendor/plugins/rails-jqgrid/public/javascripts/jqgrid"),
   File.join(RAILS_ROOT,"public/javascripts"),
   :verbose => true
 )
 
-FileUtils.ln_s(
+FileUtils.cp(
   File.join(RAILS_ROOT,"vendor/plugins/rails-jqgrid/public/stylesheets/jqgrid"),
   File.join(RAILS_ROOT,"public/stylesheets"),
   :verbose => true
--- uninstall.rb
+++ uninstall.rb
@@ -1,1 +1,16 @@
 # Uninstall hook code here
+
+FileUtils.rm_f(
+  File.join(RAILS_ROOT,"app/controllers/grid_data_controller.rb"),
+  :verbose => true
+)
+
+FileUtils.rm_rf(
+  File.join(RAILS_ROOT,"public/javascripts/jqgrid"),
+  :verbose => true
+)
+
+FileUtils.cp(
+  File.join(RAILS_ROOT,"public/stylesheets/jqgrid"),
+  :verbose => true
+)
